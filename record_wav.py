4bbb590befb972b7fd2631f18ba6c49959954a71
elenalyss <elenalyssea@gmail.com>
2025-09-09 23:07:26 +0300

    record_wav.py

--- record_wav.py
+++ record_wav.py
@@ -1,11 +1,11 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
- 
-SR = 16000      # sample rate (καλό για ASR)
-SECS = 15       # διάρκεια εγγραφής σε δευτ.
- 
-print("🎙️ Recording... μίλα τώρα (θα σταματήσει σε", SECS, "δευτερόλεπτα)")
+#We set the sample rate - 16kHz is recommended for speech recognition
+SR = 16000    
+SECS = 15 # We set the duration of the recording in seconds     
+#We  start recording from the microphone 
+print("Recording... speak now (it will stop in", SECS, "seconds)")
 audio = sd.rec(int(SECS * SR), samplerate=SR, channels=1, dtype="int16")
 sd.wait()
 write("my_report.wav", SR, audio)
-print("✅ Αποθηκεύτηκε: my_report.wav")
+print("Saved as: my_report.wav")
